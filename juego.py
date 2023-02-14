f2e18369ed7f34e4f4cb80506d0ce2e3ecab417a
CristianIglesias <iglesias.cristian99@gmail.com>
2023-02-14 17:27:09 -0300

    Armando generala -

--- juego.py
+++ juego.py
@@ -4,15 +4,15 @@
 import random
 
 #cant_Dados=6
-dados= list([random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6),random.randint(1,6)])
+dados= list([0,0,0,0,0,0])
 #while(cant_Dados!=0):
 #    cant_Dados= input("decime cuantos dados tirás rey: ")
 #
 #    if(cant_Dados>'6'):
 #        print("dale amigo, maximo 6")
 
-#for i in dados:
-#    dados[i] =  random.randint(1,6)
+for i in dados:
+    dados[i] =  random.randint(1,6)
     
 
 print(dados)
