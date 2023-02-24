644940112b26b6913fbef782e6b03db75440192f
CristianIglesias <iglesias.cristian99@gmail.com>
2023-02-24 17:15:14 -0300

    -arrancaría diccionarios

--- Variables/diccionarios2.py
+++ Variables/diccionarios2.py
@@ -2,3 +2,8 @@
 diccionario = dict(nombre="Lucas",apellido="Dalto")
 
 print(diccionario)
+
+#Creando diccionarios con Fromkeys();
+diccionario = dict.fromkeys(["nombre", "apellido"])
+print(diccionario)
+
