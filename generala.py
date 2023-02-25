522e126560428a77c4d3f1914684644b1b072105
Cristian <iglesias.cristian99@gmail.com>
2023-02-24 21:43:42 -0300

    Duplicando generalas

---
+++ generala.py
@@ -0,0 +1,75 @@
+import pygame
+#Generala? 
+import random
+
+#funcion generar random rapido
+def numeroRandom():
+    return random.randint(1,6)
+
+def mostrarDados(dados):
+    print(f'Tus dados dieron los siguientes resultados: {dados}')
+
+
+#Paradigma orientado a objetos
+
+class Dado():
+    def __init__(self):
+        self.id =0
+        self.valor=0
+        self.queda=True
+
+    def __init__(self,id):
+        self.id=id
+        self.valor=numeroRandom()
+        self.queda=True
+
+
+#se instancian los dados- hay que cargarlos con nros aleatorios
+dados_Dict = {
+    "dado1":"0",
+    "dado2":"0",
+    "dado3":"0",
+    "dado4":"0",
+    "dado5":"0",
+    "dado6":"0" }
+
+dadito1=Dado(1)
+dadito2=Dado(2)
+dadito3=Dado(3)
+dadito4=Dado(4)
+dadito6=Dado(5)
+dadito5=Dado(6)
+
+#se cargan los dados con valores random entre 1 y 6.
+for i in dados_Dict:
+    dados_Dict[i] = numeroRandom()
+
+mostrarDados(dados_Dict)
+
+dados_a_rerollear = 5
+#while dados_a_rerollear!=0:
+    # dados_a_rerollear= int(input("Cuantos dados querés cambiar campeon: "))
+
+#for i in dados:
+#    if(i<1):
+#        dados[i] = numeroRandom()
+#mostrarDados()
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+        
+    #input("que dados querés cambiar rey? ")
+
+
+
+
+
+
+
